1bcd48886433cf95625e3e80b754915345e9391a
Talyta Santos <talyta.santos@fatec.sp.gov.br>
2025-06-04 23:42:34 -0300

    update

---
+++ 1.entendendoVariaveis/variaveis_bool.py
@@ -0,0 +1,20 @@
+logica = False
+print(f"A variável contém valor {logica}")
+logica = True
+print(f"Agora a variável contém valor {logica}")
+logica = 1 > 2
+print(f"1 é maior que 2? {logica}")
+logica = 1 < 2
+print(f"1 é menor que 2? {logica}")
+logica = 1 >= 2
+print(f"1 é maior ou igual que 2? {logica}")
+logica = 1 <= 2
+print(f"1 é menor ou igual que 2? {logica}")
+logica = 1 == 2
+print(f"1 é igual a 2? {logica}")
+logica = 1 != 2
+print(f"1 é diferente de 2? {logica}")
+logica = bool(0)
+print(f"O valor de 0 convertido em lógico é {logica}")
+logica = bool(15)
+print(f"O valor de 15 convertido em lógico é {logica}")
